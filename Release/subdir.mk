b66c536207c2c98576ce5194a6a58c40b496f0fb
ellab <pcgchalkiopoulos@gmail.com>
2018-12-17 14:59:52 +0200

    change on release

--- Release/subdir.mk
+++ Release/subdir.mk
@@ -20,7 +20,7 @@ CPP_DEPS += \
 %.o: ../%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler 4 [arm-linux-gnueabihf]'
-	arm-linux-gnueabihf-g++ -std=c++0x -Dsoc_cv_av -I/MasterThesisCV/include/ -I/MasterThesisCV/include/ -I/home/ellabuser/altera/15.1/embedded/ip/altera/hps/altera_hps/hwlib/include -I/home/ellabuser/altera/15.1/embedded/ip/altera/hps/altera_hps/hwlib/include/soc_cv_av -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	arm-linux-gnueabihf-g++ -std=c++0x -Dsoc_cv_av -I/MasterThesisCV/include/opencv -I/MasterThesisCV/include/ -I/home/ellabuser/altera/15.1/embedded/ip/altera/hps/altera_hps/hwlib/include -I/home/ellabuser/altera/15.1/embedded/ip/altera/hps/altera_hps/hwlib/include/soc_cv_av -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
